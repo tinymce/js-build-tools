495455bd5ef7c076ae10b6532b66872af7e0e452
Spocke <spocke@moxiecode.com>
2009-08-19 12:45:33 +0000

    Added some documentation examples and the moxiedoc task.

--- example/example1.js
+++ example/example1.js
@@ -1,4 +1,55 @@
 (function() {
+	/**
+	 * Here is some class.
+	 *
+	 * @class SomeClass
+	 */
+	window.SomeClass = function() {
+		/**
+		 * Some function.
+		 *
+		 * @method someFunc
+		 * @param {String} arg1 First arg.
+		 * @param {String} arg2 Second arg.
+		 * @return {String} Some string.
+		 */
+		this.someFunc = function(arg1, arg2) {
+			return "";
+		};
+
+		/**
+		 * Some other function.
+		 *
+		 * @method someOtherFunc
+		 * @param {String} arg1 First arg.
+		 * @param {String} arg2 Second arg.
+		 * @return {String} Some string.
+		 */
+		this.someOtherFunc = function(arg1, arg2) {
+			return "";
+		};
+	};
+
+	/**
+	 * Here is some static class.
+	 *
+	 * @class SomeOtherClass
+	 * @static
+	 */
+	window.SomeOtherClass = {
+		/**
+		 * Some function.
+		 *
+		 * @method someFunc
+		 * @param {String} arg1 First arg.
+		 * @param {String} arg2 Second arg.
+		 * @return {String} Some string.
+		 */
+		someStaticFunc : function(arg1, arg2) {
+			return "";
+		};
+	};
+
 	var someLongVar;
 
 	function someLongFunctionName() {
